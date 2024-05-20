ccb5c26e9f40924235b8eb145cf66ca06d1d4567
udaykiran <you@example.com>
2024-05-20 09:07:33 +0530

    port changet to live server

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const vendorrouts=require("./routes/vendorrouts")
 const bodyParser=require("body-parser")
 const firmRoutes= require('./routes/firmRouts')
 const productroutes= require("./routes/productRoutes")
-const PORT=4000;
+const PORT=process.env.port||4000;
 dotEnv.config();
 const app=express()
 mongoose.connect(process.env.MONGO_URI)
@@ -20,7 +20,7 @@ app.use('/product',productroutes)
 app.listen(PORT,()=>{
     console.log(`welcome to the port ${PORT}`)
 })
-app.use('/home',(req,res)=>{
+app.use('/',(req,res)=>{
     res.send("<h1>hi this is uday</h1>")
 }
 
